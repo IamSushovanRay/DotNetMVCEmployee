14e3d5508e2ee970489b2f6a068c20ee69a6291e
raysu <raysu@SUSHOVAN_GB3>
2024-11-05 22:33:01 +0530

    Add project files.

---
+++ DotNetDemoGit/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using DotNetDemoGit.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotNetDemoGit.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly PurchaseEntities1 _dbContext = new PurchaseEntities1();
+
+        public ActionResult Index()
+        {
+            var employees = _dbContext.Employees.ToList();
+            return View(employees);
+        }
+
+    }
+}
---
+++ DotNetDemoGit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DotNetDemoGit.MvcApplication" Language="C#" %>
